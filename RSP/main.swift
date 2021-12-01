76b306411a9c870c501db2c1aaab3ebd35ccc07c
Azamat Kenjebayev <educationonline@me.com>
2021-12-01 20:40:41 +0600

    initial comit

---
+++ RSP/main.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  RSP
+//
+//  Created by Azamat Kenjebayev on 11/30/21.
+//
+
+import Foundation
+
+enum selectWeapon{
+    case Rock
+    case Scissors
+    case Paper
+    
+}
+
+enum gameStatus {
+    case Won
+    case Lose
+    case Draw
+}
+
+class PlayGame {
+    
+    func checkWinner(userPlay: selectWeapon, computerPlay: selectWeapon) -> gameStatus {
+        
+        if userPlay == computerPlay {
+            return .Draw
+        }
+        
+        if userPlay == .Rock && computerPlay == .Scissors {
+            return .Lose
+        } else if userPlay == .Rock && computerPlay == .Paper {
+            return .Won
+        }  else if userPlay == .Paper && computerPlay == .Rock {
+            return .Lose
+        } else if userPlay == .Paper && computerPlay == .Scissors {
+            return .Won
+        } else if userPlay == .Scissors && computerPlay == .Rock {
+            return .Won
+        } else if userPlay == .Scissors && computerPlay == .Paper {
+            return .Lose
+        } else {
+            return .Draw
+        }
+    }
+
+    let guessess:[selectWeapon] = [.Rock, .Scissors, .Paper]
+
+    func generateComputerGuess() -> selectWeapon {
+        return guessess.randomElement()!
+    }
+    
+
+}
+
+print("Please choose your weapon: Rock, Scissor or Paper.")
+var userChoice = readLine()
+var userGuess = userChoice
+print("Your chose \(userGuess!)")
+
