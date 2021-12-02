36b2767bae633df3c09c91899ca9b569983ae394
Azamat <educationonline@me.com>
2021-12-02 17:12:10 +0600

    Add files via upload
    final version

--- RSP/RSP/main.swift
+++ RSP/RSP/main.swift
@@ -2,63 +2,82 @@
 //  main.swift
 //  RSP
 //
-//  Created by Azamat Kenjebayev on 11/30/21.
+//  Created by Azamat Kenjebayev on 12/2/21.
 //
 
 import Foundation
 
-enum selectWeapon{
-    case Rock
-    case Scissors
-    case Paper
-    
+enum selectWeapon: String {
+    case Rock = "Rock"
+    case Scissors = "Scissors"
+    case Paper = "Paper"
 }
 
-enum gameStatus {
-    case Won
-    case Lose
-    case Draw
+enum gameStatus: String {
+    case Won = "You won!"
+    case Lost = "You lost!"
+    case Draw = "It's draw!"
 }
 
-
-print("Please choose your weapon: Rock, Scissor or Paper.")
-
-
-func checkWinner(userPlay: selectWeapon, computerPlay: selectWeapon) -> gameStatus {
-    
-    if userPlay == computerPlay {
-        return .Draw
-    }
-    
-    if userPlay == .Rock && computerPlay == .Scissors {
-        return .Lose
-    } else if userPlay == .Rock && computerPlay == .Paper {
-        return .Won
-    }  else if userPlay == .Paper && computerPlay == .Rock {
-        return .Lose
-    } else if userPlay == .Paper && computerPlay == .Scissors {
-        return .Won
-    } else if userPlay == .Scissors && computerPlay == .Rock {
-        return .Won
-    } else if userPlay == .Scissors && computerPlay == .Paper {
-        return .Lose
-    } else {
-        return .Draw
-    }
+func gameIntro(){
+    print("Hello, please choose your weapon!")
 }
+
+let botGuess = ["Rock", "Scissors", "Paper"]
+var botChoice = botGuess.randomElement()
 
 var userChoice = readLine()
-var userGuess = userChoice
-print("Your chose \(userGuess!)")
-
-
-let guessess:[selectWeapon] = [.Rock, .Scissors, .Paper]
-
-func generateComputerGuess() -> selectWeapon {
-    return guessess.randomElement()!
+func userWeapon(){
+    print(userChoice!)
 }
-print("Computer chose \(generateComputerGuess())")
 
+func botWeapon(){
+    print(botChoice!)
+}
 
-// didn't finish :(
+func checkWinner(){
+    switch botChoice {
+    case "Rock":
+        print("Bot chose Rock.")
+        if userChoice == "Rock" {
+            print(gameStatus.Draw.rawValue)
+        } else if userChoice == "Paper" {
+            print(gameStatus.Won.rawValue)
+        } else {
+            print(gameStatus.Lost.rawValue)
+        }
+        
+    case "Scissors":
+        print("Bot chose Scissors.")
+        if userChoice == "Scissors" {
+            print(gameStatus.Draw.rawValue)
+        } else if userChoice == "Rock" {
+            print(gameStatus.Won.rawValue)
+        } else {
+            print(gameStatus.Lost.rawValue)
+        }
+    case "Paper":
+        print("Bot chose Paper.")
+        if userChoice == "Paper" {
+            print(gameStatus.Draw.rawValue)
+        } else if userChoice == "Scissors" {
+            print(gameStatus.Won.rawValue)
+        } else {
+            print(gameStatus.Lost.rawValue)
+        }
+    default:
+        print("Something gone wrong.")
+    }
+}
 
+func playGame(){
+    for i in 1...3 {
+        print("Round \(i). Chose your weapon: Rock, Scissors or Paper.")
+        userWeapon()
+        botWeapon()
+        checkWinner()
+    }
+}
+
+gameIntro()
+playGame()
